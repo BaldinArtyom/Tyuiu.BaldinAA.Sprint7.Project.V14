328c143061fca3a3d0051b63f81be9e6614968be
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-12-22 22:13:01 +0500

    Add project files.

---
+++ Tyuiu.BaldinAA.Sprint7.Project.V14.Test/DataServiceTestBase.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+
+
+namespace Tyuiu.BaldinAA.Sprint7.Project.V14.Test
+{
+    [TestClass]
+    public abstract class DataServiceTestBase
+    {
+        public abstract void TestMethod();
+    }
+}
